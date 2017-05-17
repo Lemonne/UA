540252e106c6a1638cd7b2cdd104bdece5c0ba21
Nerissa Lemon <Nerissa@Nerissas-MacBook-Pro.local>
2017-05-17 14:45:25 -0700

    CRLF working?

--- finger.py
+++ finger.py
@@ -7,12 +7,25 @@ serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serversocket.bind(('localhost', 8089))
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
+# read user data, handle timeouts.
+# check that completed data ends with CR/LF
+# return line for parsing or None
+def getreq(c):
+    l = c.recv(2048)
+    sz = len(l)
+    if l[sz-2:] == "\r\n":
+        print "CRLF"
+    print "last", l[sz-2]
+    return l
+
 while True:
   c, addr = serversocket.accept()
   print "connection from", addr
-  l = c.recv(2048)
+  l = getreq(c)
   print "have", l
-  
+  #need to parse l, for line.
+
+
 
   c.close()
 
